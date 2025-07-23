5c0f7766096901d7365bdf0b307b92688b75f686
pwngithub <110849398+pwngithub@users.noreply.github.com>
2025-07-23 12:51:44 -0400

    Add files via upload

--- construction.py
+++ construction.py
@@ -20,8 +20,7 @@ def run_construction_dashboard():
     )
 
     st.image("https://www.pioneerbroadband.net/sites/all/themes/pioneer/images/logo.png", width=300)
-st.markdown("""<div style="text-align:center;"><img src='https://images.squarespace-cdn.com/content/v1/651eb4433b13e72c1034f375/369c5df0-5363-4827-b041-1add0367f447/PBB+long+logo.png?format=1500w' width="600"></div>""", unsafe_allow_html=True)
-st.title("Construction Dashboard")
+    st.title("Construction Dashboard")
 
     def load_from_jotform():
         api_key = "22179825a79dba61013e4fc3b9d30fa4"
@@ -30,7 +29,7 @@ st.title("Construction Dashboard")
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-
+        
         submissions = []
         for item in data["content"]:
             answers = item.get("answers", {})
@@ -42,7 +41,7 @@ st.title("Construction Dashboard")
                 if name and answer is not None:
                     record[name] = answer
             submissions.append(record)
-
+        
         df = pd.DataFrame(submissions)
         return df
 
