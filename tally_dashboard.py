d1b41b57a4c631b8c58c7c8f72b779a10c3162cb
pwngithub <110849398+pwngithub@users.noreply.github.com>
2025-07-24 09:33:06 -0400

    Add files via upload

--- tally_dashboard.py
+++ tally_dashboard.py
@@ -6,7 +6,7 @@ from utils import fetch_jotform_data, preprocess_data
 FORM_ID = "240073839937062"  # Replace with actual tally form ID
 
 def load_data():
-    data = fetch_jotform_data(FORM_ID)
+    data = fetch_jotform_data(FORM_ID, 'ba9013143bfda3a448297144c0527f7e')
     df = preprocess_data(data)
     return df
 
